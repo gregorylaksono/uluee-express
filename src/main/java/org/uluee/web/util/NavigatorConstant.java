5270156c9217552e10287273cf4998d40dba8837
gregory laksono <gregory.laksono@mcpayment.co.id>
2017-08-18 17:10:02 +0700

    asd

--- src/main/java/org/uluee/web/util/NavigatorConstant.java
+++ src/main/java/org/uluee/web/util/NavigatorConstant.java
@@ -4,4 +4,5 @@ public class NavigatorConstant {
 
 	public static final String MAIN_PAGE= "main";
 	public static final String BOOKING_PAGE = "booking";
+	public static final String CONFIRM_PAGE = "confirm";
 }
