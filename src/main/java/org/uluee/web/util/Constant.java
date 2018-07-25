6d80b97c950b0acbb75cecc8536e0399a7a12777
gregory laksono <greg.laksono@gmail.com>
2018-07-25 14:49:40 +0700

    testing all

--- src/main/java/org/uluee/web/util/Constant.java
+++ src/main/java/org/uluee/web/util/Constant.java
@@ -2,10 +2,10 @@ package org.uluee.web.util;
 	
 public class Constant {
 
-	public static final String USERNAME = "act001";
-	public static final String PASSWORD = "ffw";
+	public static final String USERNAME = "spice";
+	public static final String PASSWORD = "spice";
 //	public static final String URL = "http://10.10.6.3";
-	public static final String URL = "http://acternity.net";
+	public static final String URL = "http://localhost:8888";
 	public static final String WS_PATH = URL+"/WEBSERVICE/services/MobileService?wsdl";
 	
 	
@@ -16,7 +16,7 @@ public class Constant {
 	public static final String OUT_JSON = "/json";
 	public static final String API_KEY = "AIzaSyAFj7YunZys5V1taEviGXN6p6-bc2McR9M";
 	public static final String GMAP_API_KEY = "AIzaSyBtcT4UkbfN9JV_4haAAYdQnhS-3wsedpk";
-	public static final String returnPaypalUrl = "http://acternity.net/#!confirm/";
-	public static final String cancelPaypalUrl = "http://acternity.net/#!cancel/";
+	public static final String returnPaypalUrl = URL+"/#!confirm/";
+	public static final String cancelPaypalUrl = URL+"/#!cancel/";
 }
 
